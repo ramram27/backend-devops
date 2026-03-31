f376a3f71ee0960d66fa98a6977814e381806b1f
Rohit <rohitkumar@caplineservices.com>
2026-03-31 21:31:25 +0530

    test

--- tests/app.test.js
+++ tests/app.test.js
@@ -3,12 +3,12 @@ const app = require('../index');
 
 describe('Math API Tests', () => {
 
-  test('GET / Hello World', async () => {
-    const res = await request(app).get('/');
-    expect(res.statusCode).toBe(200);
-    expect(res.text).toBe('Hello World!, Hi');
-  });
-
+   test('GET/should test msg', async() => { 
+     const res = await request(app).get('/')
+     expect(res.statusCode).toBe(200);
+     expect(res.text).toBe('Hello World!, Hi');
+   })
+   
 });
 
 

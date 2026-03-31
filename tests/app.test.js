8b38824987bd4452d6a938dcb16dd3be24b83c74
Rohit <rohitkumar@caplineservices.com>
2026-03-31 14:24:49 +0530

    test

--- tests/app.test.js
+++ tests/app.test.js
@@ -6,7 +6,7 @@ describe('Math API Tests', () => {
   test('GET / should return Hello World', async () => {
     const res = await request(app).get('/');
     expect(res.statusCode).toBe(200);
-    expect(res.text).toBe('Hello World!');
+    expect(res.text).toBe('Hello World!, Welcome to the DevOps Backend Server, This is a simple Express application for testing purposes.');
   });
 
   // ➕ ADD
